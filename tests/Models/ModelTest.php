89c22cc33f43bf5c5eba168680a80f769b586f9d
José Miguel Moreno <josemmo@pm.me>
2025-06-21 13:56:18 +0200

    Added initial unit tests

---
+++ tests/Models/ModelTest.php
@@ -0,0 +1,36 @@
+<?php
+namespace Verifactu\Tests\Models;
+
+use PHPUnit\Framework\Attributes\DoesNotPerformAssertions;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\Validator\Constraints as Assert;
+use Verifactu\Exceptions\InvalidModelException;
+use Verifactu\Models\Model;
+
+class SampleModel extends Model {
+    #[Assert\NotBlank]
+    #[Assert\Length(exactly: 4)]
+    public string $name;
+
+    #[Assert\NotBlank]
+    #[Assert\Positive]
+    public int $quantity;
+}
+
+final class ModelTest extends TestCase {
+    #[DoesNotPerformAssertions]
+    public function testNotThrowsOnValidModel(): void {
+        $model = new SampleModel();
+        $model->name = 'abcd';
+        $model->quantity = 2;
+        $model->validate();
+    }
+
+    public function testThrowsOnInvalidModel(): void {
+        $this->expectException(InvalidModelException::class);
+        $model = new SampleModel();
+        $model->name = 'This is not a valid name';
+        $model->quantity = 0;
+        $model->validate();
+    }
+}
---
+++ tests/Models/Records/RegistrationRecordTest.php
@@ -0,0 +1,69 @@
+<?php
+namespace Verifactu\Tests\Models\Records;
+
+use DateTimeImmutable;
+use PHPUnit\Framework\TestCase;
+use Verifactu\Models\Records\BreakdownDetails;
+use Verifactu\Models\Records\InvoiceIdentifier;
+use Verifactu\Models\Records\InvoiceType;
+use Verifactu\Models\Records\OperationType;
+use Verifactu\Models\Records\RegimeType;
+use Verifactu\Models\Records\RegistrationRecord;
+use Verifactu\Models\Records\TaxType;
+
+final class RegistrationRecordTest extends TestCase {
+    public function testCalculatesHashForFirstRecord(): void {
+        $record = new RegistrationRecord();
+        $record->invoiceId = new InvoiceIdentifier();
+        $record->invoiceId->issuerId = 'A00000000';
+        $record->invoiceId->invoiceNumber = 'PRUEBA-0001';
+        $record->invoiceId->issueDate = new DateTimeImmutable('2025-06-01');
+        $record->issuerName = 'Perico de los Palotes, S.A.';
+        $record->invoiceType = InvoiceType::Simplificada;
+        $record->description = 'Factura simplificada de prueba';
+        $record->breakdown[0] = new BreakdownDetails();
+        $record->breakdown[0]->taxType = TaxType::IVA;
+        $record->breakdown[0]->regimeType = RegimeType::C01;
+        $record->breakdown[0]->operationType = OperationType::S1;
+        $record->breakdown[0]->taxRate = '21.00';
+        $record->breakdown[0]->baseAmount = '10.00';
+        $record->breakdown[0]->taxAmount = '2.10';
+        $record->totalTaxAmount = '2.10';
+        $record->totalAmount = '12.10';
+        $record->previousInvoiceId = null;
+        $record->previousHash = null;
+        $record->hashedAt = new DateTimeImmutable('2025-06-01T10:20:30+02:00');
+        $record->hash = $record->calculateHash();
+        $this->assertEquals('F223F0A84F7D0C701C13C97CF10A1628FF9E46A003DDAEF3A804FBD799D82070', $record->hash);
+        $record->validate();
+    }
+
+    public function testCalculatesHashForOtherRecords(): void {
+        $record = new RegistrationRecord();
+        $record->invoiceId = new InvoiceIdentifier();
+        $record->invoiceId->issuerId = 'A00000000';
+        $record->invoiceId->invoiceNumber = 'PRUEBA-0002';
+        $record->invoiceId->issueDate = new DateTimeImmutable('2025-06-02');
+        $record->issuerName = 'Perico de los Palotes, S.A.';
+        $record->invoiceType = InvoiceType::Simplificada;
+        $record->description = 'Factura simplificada de prueba';
+        $record->breakdown[0] = new BreakdownDetails();
+        $record->breakdown[0]->taxType = TaxType::IVA;
+        $record->breakdown[0]->regimeType = RegimeType::C01;
+        $record->breakdown[0]->operationType = OperationType::S1;
+        $record->breakdown[0]->taxRate = '21.00';
+        $record->breakdown[0]->baseAmount = '100.00';
+        $record->breakdown[0]->taxAmount = '21.00';
+        $record->totalTaxAmount = '21.00';
+        $record->totalAmount = '121.00';
+        $record->previousInvoiceId = new InvoiceIdentifier();
+        $record->previousInvoiceId->issuerId = 'A00000000';
+        $record->previousInvoiceId->invoiceNumber = 'PRUEBA-001';
+        $record->previousInvoiceId->issueDate = new DateTimeImmutable('2025-06-01');
+        $record->previousHash = 'AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA';
+        $record->hashedAt = new DateTimeImmutable('2025-06-02T20:30:40+02:00');
+        $record->hash = $record->calculateHash();
+        $this->assertEquals('4566062C5A5D7DA4E0E876C0994071CD807962629F8D3C1F33B91EDAA65B2BA1', $record->hash);
+        $record->validate();
+    }
+}
